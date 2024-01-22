aa753ebc18f61809ddab964c14fcf44a3060aa9e
kaja-shereef <157398343+kaja-shereef@users.noreply.github.com>
2024-01-22 21:58:19 +0530

    add initial portfolio website

---
+++ index.js
@@ -0,0 +1,16 @@
+// var button = document.getElementById("btn")
+//function lightmode(){
+  //  Element.style.backgroundColor= "#fff"
+
+   // }
+    var button = document.getElementById("btn");
+
+    function lightmode() {
+      document.body.style.backgroundColor=("#fff")
+    }
+
+    var button = document.getElementById("#dark")
+    function darkmode() {
+      document.body.style.backgroundColor=("tan")
+    }
+    
